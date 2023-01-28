29fd850b30b90126290b305fcf2c99a20f8657a8
yuri8392 <yuri.yamaguchi.28@gmail.com>
2023-01-28 15:00:23 +0900

    ユーザーテーブルカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_24_141047) do
+ActiveRecord::Schema.define(version: 2023_01_28_054132) do
 
   create_table "apartments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "apartment_number", null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2023_01_24_141047) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", default: "", null: false
+    t.boolean "admin", default: false
     t.index ["employee_number"], name: "index_users_on_employee_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
