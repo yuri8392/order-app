3434b3e7f462d67457401b07a977892d0b418a1e
yuri8392 <yuri.yamaguchi.28@gmail.com>
2023-02-14 23:34:55 +0900

    Capistranoの導入準備修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
-server '52.198.130.67', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -60,3 +59,4 @@ server '52.198.130.67', user: 'ec2-user', roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '52.198.130.67', user: 'ec2-user', roles: %w{app db web}
