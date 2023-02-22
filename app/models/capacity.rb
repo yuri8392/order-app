ba9f00e1dc5f13a1dbb7ab17e7a3a0d2d3d626dd
yuri8392 <yuri.yamaguchi.28@gmail.com>
2023-02-22 22:37:51 +0900

    コントローラービュー作成

--- app/models/capacity.rb
+++ app/models/capacity.rb
@@ -1,2 +1,5 @@
 class Capacity < ApplicationRecord
+  validates :client_number, presence: true
+  validates :client_name, presence: true
+  validates :capacity, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "memos#index"
   resources :memos, only: [:index, :new, :create, :destroy,:edit,:update]
-  resources :capacities, only: :index
+  resources :capacities, only: [:index, :new, :create]
 end
