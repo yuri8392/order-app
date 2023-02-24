d4bd7d171e6ce4e9b29850c85ddea976d4c17004
yuri8392 <yuri.yamaguchi.28@gmail.com>
2023-02-24 21:52:49 +0900

    コントローラービュー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,14 @@
+class OrdersController < ApplicationController
+  before_action :move_to_index
+  
+  def index
+    @orders = Order.all
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+end
