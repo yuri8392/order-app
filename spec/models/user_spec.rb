78405756cc90956db8a220b0776a79bfa0f1d73f
yuri8392 <yuri.yamaguchi.28@gmail.com>
2023-02-28 22:53:44 +0900

    Rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe User, type: :model do
       @user.password = Faker::Internet.password(min_length: 129)
       @user.password_confirmation = @user.password
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
+      expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
     end
   end
 end
