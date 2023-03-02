d04323236049757f120b2ef4cbe704f540c03701
yuri8392 <yuri.yamaguchi.28@gmail.com>
2023-03-03 00:47:02 +0900

    rubocop

--- spec/factories/capacities.rb
+++ spec/factories/capacities.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :capacity do
-    client_number {Faker::Number.between(from: 10000)}
-    client_name {Faker::Company.name}
-    capacity {Faker::Number.between(from: 0)} 
+    client_number { Faker::Number.between(from: 1000, to: 9999) }
+    client_name { Faker::Company.name }
+    capacity { Faker::Number.between(from: 0, to: 999) }
   end
 end
